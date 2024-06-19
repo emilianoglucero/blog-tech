6da06e53c56ef5b5740e5843f8ee17dca1e71e83
emiliano lucero <emilianoglucero@gmail.com>
2024-06-19 11:45:34 -0300

    feat: Improve CSS styles for welcome module, add blog post container CSS module, loader component, TransitionLink component, and banner
    This commit includes multiple code changes:
    - Updated CSS styles in the `welcome.module.css` file to improve the styling and layout of the welcome section on the page.
    - Added a new CSS module `blogpost.module.css` to the `src/app/[slug]` directory for the blog post container, improving the styling and organization of the blog post page.
    - Added a new file `loader/index.tsx` and its corresponding CSS module `loader.module.css` to the `src/components` directory. The `Loader` component displays a loading animation while the page is being loaded, improving the user experience.
    - Added a new file `page.tsx` in the `src/components/transition-link` directory for the `TransitionLink` component, which provides smooth transitions between pages.
    - Added the `introSeen` and `assetsLoaded` properties to the `AppStore` interface in the `use-app-store.ts` file, allowing for tracking whether the intro has been seen and whether the assets have been loaded in the application.
    - Updated the global.scss file with new primary and secondary colors.
    - Added blog post data and types in the `blog-posts.ts` file in the `src/data` directory, including dummy data and properties like `id`, `title`, `subtitle`, `date`, `slug`, `content`, `photo`, `photo_width`, `photo_height`, and `margin_top`.
    - Added animations for homepage and post page transitions in the `animations.ts` file.
    - Added a new CSS class `.banner` to the `template.module.css` file for a fixed position and styled banner that acts as a screen to display animations.
    - Added a homepage component that conditionally renders the welcome or the loader component.
    
    These changes enhance the styling, organization, user experience, and functionality of the application.

--- src/app/sections/welcome/index.tsx
+++ src/app/sections/welcome/index.tsx
@@ -1,113 +1,213 @@
 'use client'
+import { useGSAP } from '@gsap/react'
 import Image from 'next/image'
-import React, { useRef, useState } from 'react'
+import Link from 'next/link'
+import React, { useRef } from 'react'
 
-import useIsomorphicLayoutEffect from '~/hooks/use-isomorphic-layout'
+import TransitionLink from '~/components/transition-link/page'
+import blogPosts from '~/data/blog-posts'
 import { gsap } from '~/lib/gsap'
 
 import authorPic from '../../images/author/emi.jpg'
-import blogPostNetArt from '../../images/blogposts/netart-post.jpg'
 import s from './welcome.module.css'
 
 export const Welcome = () => {
-  const [isBlogPostHovered, setIsBlogPostHovered] = useState(false)
-  const [isAuthorHovered, setIsAuthorHovered] = useState(false)
   const authorPhotoRef = useRef<HTMLDivElement>(null)
-  const blogPostPhotoRef = useRef<HTMLDivElement>(null)
+  const blogImagesRef = useRef<Record<number, HTMLDivElement | null>>({})
+  const titleRef = useRef<HTMLHeadingElement>(null)
+  const subtitleRef = useRef<HTMLDivElement>(null)
+  const descriptionRef = useRef<HTMLParagraphElement>(null)
+  const linksRef = useRef<HTMLUListElement>(null)
+  const linksContainerRef = useRef<HTMLDivElement>(null)
+  const subtitleContainer = useRef<HTMLDivElement>(null)
 
-  useIsomorphicLayoutEffect(() => {
-    if (!blogPostPhotoRef.current) return
-    if (isBlogPostHovered) {
-      gsap.to(blogPostPhotoRef.current, {
-        autoAlpha: 1
-      })
-    }
-    if (!isBlogPostHovered) {
-      gsap.to(blogPostPhotoRef.current, {
-        autoAlpha: 0
-      })
-    }
-  }, [isBlogPostHovered])
-  useIsomorphicLayoutEffect(() => {
-    if (!authorPhotoRef.current) return
+  const authorNameRef = useRef<HTMLParagraphElement>(null)
+  const authorDescriptionRef = useRef<HTMLParagraphElement>(null)
+  const authorEmailRef = useRef<HTMLParagraphElement>(null)
 
-    if (isAuthorHovered) {
-      gsap.to(authorPhotoRef.current, {
-        autoAlpha: 1
+  const handleBlogHover = (e: React.MouseEvent<HTMLElement>, id: number) => {
+    const element = blogImagesRef.current[id]
+    if (element) {
+      gsap.to(element, {
+        autoAlpha: e ? 1 : 0
       })
     }
-    if (!isAuthorHovered) {
-      gsap.to(authorPhotoRef.current, {
-        autoAlpha: 0
+  }
+
+  const handleBlogHoverExit = (
+    e: React.MouseEvent<HTMLElement>,
+    id: number
+  ) => {
+    const element = blogImagesRef.current[id]
+    if (element) {
+      gsap.to(element, {
+        autoAlpha: e ? 0 : 1
       })
     }
-  }, [isAuthorHovered])
+  }
+
+  const handleAuthorHover = (e: any) => {
+    if (authorPhotoRef.current) {
+      gsap.to(authorPhotoRef.current, {
+        autoAlpha: e ? 1 : 0
+      })
+    }
+  }
+
+  const handleAuthorHoverExit = (e: any) => {
+    gsap.to(authorPhotoRef.current, {
+      autoAlpha: e ? 0 : 1
+    })
+  }
+
+  useGSAP(() => {
+    if (
+      !titleRef.current &&
+      !subtitleRef.current &&
+      !descriptionRef.current &&
+      !linksRef.current
+    ) {
+      return
+    }
+
+    gsap.from([titleRef.current, subtitleRef.current, descriptionRef.current], {
+      yPercent: 130,
+      delay: 0.1,
+      stagger: 0.1,
+      onComplete: () => {
+        gsap.set(subtitleContainer.current, {
+          overflow: 'visible'
+        })
+        gsap.set(subtitleRef.current, {
+          display: 'block'
+        })
+      }
+    })
+
+    gsap.from(linksRef.current, {
+      yPercent: 130,
+      delay: 0.1,
+      stagger: 0.1,
+      onComplete: () => {
+        gsap.set(linksContainerRef.current, {
+          overflow: 'visible'
+        })
+        gsap.set(linksRef.current, {
+          display: 'block'
+        })
+      }
+    })
+
+    gsap.from(
+      [
+        authorNameRef.current,
+        authorDescriptionRef.current,
+        authorEmailRef.current
+      ],
+      {
+        yPercent: 130,
+        delay: 0.1,
+        stagger: 0.1
+      }
+    )
+  }, [])
 
   return (
     <div className={s.welcome}>
-      <h1 className={s.title}>digital digital</h1>
-      <div className={s.subtitle}>
-        <div>
-          by{' '}
-          <a
-            href="https://www.emilianolucero.ar"
-            target="_blank"
-            rel="noopener"
-            onMouseEnter={() => setIsAuthorHovered(true)}
-            onMouseLeave={() => setIsAuthorHovered(false)}
-          >
-            <span className={s.underline}>emi</span>
-          </a>
-          <div className={s.authorPic} ref={authorPhotoRef}>
-            <Image
-              src={authorPic}
-              alt="Photo of the author of the blog - Emiliano Lucero"
-              style={{
-                width: 'auto',
-                height: '600px'
-              }}
-            />
+      <div className={s.header}>
+        <div className={s.titles}>
+          <h1 className={s.title}>
+            <span ref={titleRef}>I 🖤 my computer job</span>
+          </h1>
+          <div className={s.subtitle} ref={subtitleContainer}>
+            <div ref={subtitleRef}>
+              by{' '}
+              <Link
+                href="https://www.emilianolucero.ar"
+                target="_blank"
+                rel="noopener"
+              >
+                <span
+                  className={s.underline}
+                  onMouseEnter={(e) => handleAuthorHover(e)}
+                  onMouseLeave={(e) => handleAuthorHoverExit(e)}
+                >
+                  emi
+                </span>
+              </Link>
+              <div className={s.authorPic} ref={authorPhotoRef}>
+                <Image
+                  src={authorPic}
+                  alt="Photo of the author of the blog - Emiliano Lucero"
+                  style={{
+                    width: 'auto',
+                    height: '600px'
+                  }}
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className={s.description}>
-        <p>writings, explorations and stuff.</p>
+        <div className={s.description}>
+          <p ref={descriptionRef}>writings, explorations and stuff.</p>
+        </div>
       </div>
       <div className={s.links}>
-        <ul>
-          <li>
-            <a
-              href="#"
-              onMouseEnter={() => setIsBlogPostHovered(true)}
-              onMouseLeave={() => setIsBlogPostHovered(false)}
-            >
-              <h2 className={s.blog_post_netart_text}>
-                Los años y tu proyecto personal, pensamientos sobre como
-                mantanerlo en el tiempo de manera austera y funcional, December
-                11, 2023
-              </h2>
-            </a>
-
-            <div className={s.blog_post_netart_image} ref={blogPostPhotoRef}>
-              <Image
-                src={blogPostNetArt}
-                alt="Photo of a group of mens playing cards in the beach related to the blog post about net art"
-                style={{
-                  width: '680px',
-                  height: 'auto'
-                }}
-              />
-            </div>
-          </li>
-        </ul>
+        <div ref={linksContainerRef}>
+          <ul ref={linksRef}>
+            {blogPosts.map((post) => (
+              <div key={post.id}>
+                {' '}
+                {/* Add a div with a key prop here */}
+                <li>
+                  <TransitionLink href={`/${post.slug}`}>
+                    <h2
+                      className={s.blog_post_text}
+                      onMouseEnter={(e) => handleBlogHover(e, post.id)}
+                      onMouseLeave={(e) => handleBlogHoverExit(e, post.id)}
+                    >
+                      {post.title}, {post.date}
+                    </h2>
+                  </TransitionLink>
+                </li>
+                <div
+                  className={s.blog_post_image}
+                  ref={(el) => {
+                    blogImagesRef.current[post.id] = el
+                  }}
+                >
+                  <Image
+                    src={post.photo}
+                    alt="Photo of a group of mens playing cards in the beach related to the blog post about net art"
+                    width={post.photo_width}
+                    height={post.photo_height}
+                    style={{
+                      float: 'right',
+                      marginTop: `${post.margin_top}%`
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className={s.info}>
-        <p className={s.author_name}>Emiliano Lucero</p>
-        <p className={s.author_description}>
-          A software engineer based in Rosario, Argentina.
-        </p>
-        <p className={s.author_email}>emilianoglucero@gmail.com</p>
+      <div className={s.footer}>
+        <div>
+          <p className={s.author_name} ref={authorNameRef}>
+            Emiliano Lucero
+          </p>
+        </div>
+        <div>
+          <p className={s.author_description} ref={authorDescriptionRef}>
+            A software engineer based in Rosario, Argentina.
+          </p>
+        </div>
+        <div>
+          <p className={s.author_email} ref={authorEmailRef}>
+            emilianoglucero@gmail.com
+          </p>
+        </div>
       </div>
     </div>
   )
