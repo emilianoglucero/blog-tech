5a3ba168a2aec8f67611fbd39a2ad4952a20b49a
emiliano lucero <emilianoglucero@gmail.com>
2024-06-19 11:34:57 -0300

    feat: Add introSeen and assetsLoaded to AppStore
    This commit adds the `introSeen` and `assetsLoaded` properties to the `AppStore` interface in the `use-app-store.ts` file. It also includes the corresponding `setIntroSeen` and `setAssetsLoaded` methods for updating these properties. This change allows for tracking whether the intro has been seen and whether the assets have been loaded in the application.

--- src/context/use-app-store.ts
+++ src/context/use-app-store.ts
@@ -5,9 +5,18 @@ import { create } from 'zustand'
 export interface AppStore {
   fontsLoaded: boolean
   setFontsLoaded: (fontsLoaded: boolean) => void
+  introSeen: boolean
+  setIntroSeen: (introSeen: boolean) => void
+  assetsLoaded: boolean
+  setAssetsLoaded: (assetsLoaded: boolean) => void
 }
 
 export const useAppStore = create<AppStore>((set) => ({
   fontsLoaded: false,
-  setFontsLoaded: (fontsLoaded: boolean) => set((s) => ({ ...s, fontsLoaded }))
+  setFontsLoaded: (fontsLoaded: boolean) => set((s) => ({ ...s, fontsLoaded })),
+  introSeen: false,
+  setIntroSeen: (introSeen: boolean) => set((s) => ({ ...s, introSeen })),
+  assetsLoaded: false,
+  setAssetsLoaded: (assetsLoaded: boolean) =>
+    set((s) => ({ ...s, assetsLoaded }))
 }))
