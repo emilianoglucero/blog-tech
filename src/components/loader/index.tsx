59203d47c36abb361441f5edaf9ff3d50f42894f
emiliano lucero <emilianoglucero@gmail.com>
2024-06-19 11:36:28 -0300

    feat: Add loader component for page loading
    This commit adds a new file `loader/index.tsx` and its corresponding CSS module `loader.module.css` to the `src/components` directory. The `loader/index.tsx` file contains the implementation of the `Loader` component, which displays a loading animation while the page is being loaded. The component uses the `useGSAP` hook from the `@gsap/react` package to animate the loading symbol. It also utilizes the `useAppStore` hook from the `~/context/use-app-store` module to track whether the intro has been seen. The `Loader` component is included in the `Template` component in the `template.tsx` file. This change improves the user experience by providing visual feedback during page loading.

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+
+import { useAppStore } from '~/context/use-app-store'
+import { gsap } from '~/lib/gsap'
+
+import s from './loader.module.css'
+
+export const Loader = () => {
+  const symbolRef = useRef<HTMLSpanElement | null>(null)
+  const containerRef = useRef<HTMLDivElement | null>(null)
+  const { introSeen, setIntroSeen } = useAppStore()
+
+  useGSAP(() => {
+    if (!symbolRef.current && !introSeen) return
+
+    const tl = gsap.timeline()
+
+    tl.to(symbolRef.current, {
+      yPercent: 130,
+      duration: 1.8,
+      delay: 0.4
+    }).to(containerRef.current, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: 'power4.inOut',
+      onComplete: () => {
+        setIntroSeen(true)
+      }
+    })
+  }, [introSeen, setIntroSeen])
+
+  return (
+    <div className={s.container} ref={containerRef}>
+      <div className={s.symbols_container}>
+        <span className={s.symbol_character} ref={symbolRef}>
+          ✦
+        </span>
+      </div>
+    </div>
+  )
+}
