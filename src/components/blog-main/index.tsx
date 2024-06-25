b37463b794b3f1e895a7d6072d1e2b1410fddc9e
emiliano lucero <emilianoglucero@gmail.com>
2024-06-24 21:31:32 -0300

    blog component added to run in the client and have the capability to use the hooks and conditional rendering

---
+++ src/components/blog-main/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Welcome } from '~/app/sections/welcome'
+import { useAppStore } from '~/context/use-app-store'
+import { Post } from '~/lib/payload-types'
+
+import { Loader } from '../loader'
+
+const BlogMain: React.FC<{ posts: Post[] }> = ({ posts }) => {
+  const { introSeen } = useAppStore()
+  return introSeen ? <Welcome posts={posts} /> : <Loader />
+}
+
+export default BlogMain
