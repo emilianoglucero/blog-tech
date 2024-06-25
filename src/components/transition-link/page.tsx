b751b41bfdbff9864853e499f30bf0f5fb0af918
emiliano lucero <emilianoglucero@gmail.com>
2024-06-24 21:25:49 -0300

    - transitionlink converted to <a> from button to be more organic with the styles

--- src/components/transition-link/page.tsx
+++ src/components/transition-link/page.tsx
@@ -4,6 +4,8 @@ import { usePathname, useRouter } from 'next/navigation'
 
 import { animateHomePageOut, animatePostPageOut } from '~/utils/animations'
 
+import s from './transition-link.module.css'
+
 interface TransitionLinkProps {
   href: string
   children: React.ReactNode
@@ -26,7 +28,11 @@ const TransitionLink = ({ href, children }: TransitionLinkProps) => {
     }
   }
 
-  return <button onClick={handleClick}>{children}</button>
+  return (
+    <a className={s.transition__link} onClick={handleClick}>
+      {children}
+    </a>
+  )
 }
 
 export default TransitionLink
