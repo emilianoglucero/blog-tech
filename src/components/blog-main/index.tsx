3c7d566d9ab41bedfac4fb4a75fe9cd8598a0526
emiliano lucero <emilianoglucero@gmail.com>
2024-07-17 23:05:14 -0300

    - welcome component lazy loaded with dynamic was causing a blank screen, updated to loadin the normal way

--- src/components/blog-main/index.tsx
+++ src/components/blog-main/index.tsx
@@ -1,14 +1,33 @@
 'use client'
 
-import { Welcome } from '~/app/sections/welcome'
+import Welcome from '~/app/sections/welcome'
+// import Welcome from '~/app/sections/welcome'
 import { useAppStore } from '~/context/use-app-store'
 import { PostsResponse } from '~/lib/payload-types'
 
-import { Loader } from '../loader'
+import Loader from '../loader'
+
+// const Welcome = lazy(() =>
+//   import('~/app/sections/welcome').then((module) => ({
+//     default: module.default
+//   }))
+// )
+
+// const Welcome = dynamic(() => import('~/app/sections/welcome'), {
+//   suspense: true,
+//   loading: () => null
+// })
 
 const BlogMain: React.FC<{ posts: PostsResponse }> = ({ posts }) => {
   const { introSeen } = useAppStore()
+  // if (!introSeen) {
+  //   return <Loader />
+  // }
+  // return <Welcome posts={posts} />
+
+  // return <Loader />
   return introSeen ? <Welcome posts={posts} /> : <Loader />
+  // return <Welcome posts={posts} />
 }
 
 export default BlogMain
