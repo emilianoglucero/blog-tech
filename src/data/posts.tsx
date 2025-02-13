9ff2a9d3275df75c9b0da8939a7290d074ff0b5a
emiliano lucero <emilianoglucero@gmail.com>
2025-02-13 12:10:39 -0300

    refactor: Update post metadata and image details in posts data

--- src/data/posts.tsx
+++ src/data/posts.tsx
@@ -1,20 +1,5 @@
 export const POSTS = [
   {
-    title: 'October-November (2024)',
-    subtitle:
-      'Ambient Textures: A curated selection of sounds every two-months',
-    dateToShow: 'September 19, 2024',
-    slug: 'october-november-24',
-    meta: {
-      image: {
-        url: '/assets/images/not-forever-art/netart-post.jpg',
-        alt: 'Captured in Porto, Portugal (2023): a group of elder men enjoying a game of cards in the warm sand.',
-        width: '1642',
-        height: '1094'
-      }
-    }
-  },
-  {
     title: 'Not Forever Art',
     subtitle:
       'Some Thoughts About Maintaining My Net Art Project Through the Years.',
@@ -22,13 +7,42 @@ export const POSTS = [
     slug: 'not-forever-art',
     meta: {
       image: {
-        url: '/assets/images/playlist-oct-nov/playlist-oct-nov.jpg',
-        alt: 'Photo of a group of people in a concert.',
+        url: '/assets/images/not-forever-art/netart-post.jpg',
+        alt: 'Captured in Porto, Portugal (2023): a group of elder men enjoying a game of cards in the warm sand.',
         width: '1610',
         height: '10734'
       }
     }
   },
+  {
+    title: 'October-November (2024)',
+    subtitle:
+      'Ambient Textures: A curated selection of sounds every two-months',
+    dateToShow: 'September 19, 2024',
+    slug: 'october-november-24',
+    meta: {
+      image: {
+        url: '/assets/images/playlist-oct-nov/playlist-oct-nov.jpg',
+        alt: 'Capture in Barcelona, Spain (2023): a group of kids in a concert.',
+        width: '1610',
+        height: '10734'
+      }
+    }
+  },
+  // {
+  //   title: 'December - January (2024/5)',
+  //   subtitle: 'A delayed one for the holidays.',
+  //   dateToShow: 'February 10, 2025.',
+  //   slug: 'december-january-24-5',
+  //   meta: {
+  //     image: {
+  //       url: '/assets/images/playlist-dec-jan/cocina.jpg',
+  //       alt: 'Capture in Ciudad de Córdoba, Argentina (2024): a girl with a bass in a kitchen.',
+  //       width: '1642',
+  //       height: '1046'
+  //     }
+  //   }
+  // },
   {
     title: '2024',
     subtitle: 'Highlights of the year.',
@@ -37,9 +51,9 @@ export const POSTS = [
     meta: {
       image: {
         url: '/assets/images/2024-highlights/heart.jpg',
-        alt: 'Photo of a group of people in a concert.',
-        width: '1610',
-        height: '10734'
+        alt: 'Capture in Ciudad de Córdoba, Argentina (2024): a photo of a heart balloon in the street.',
+        width: '1642',
+        height: '1094'
       }
     }
   }
